01bf84e3c264ffae10d0d74b2b6db25ec0215f86
おんJ民 <J onjmin@users.noreply.github.com>
2026-06-08 01:15:50 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/unj-reze",
+  assetPrefix: "/unj-reze/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
